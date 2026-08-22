915eac430698cc5c02dd40313b19590b152aa26f
Sumit4374 <kusumit437@gmail.com>
2026-08-22 23:55:08 +0530

    springboot initialization

---
+++ flash_scale/src/main/java/sumit/flashscale/flash_scale/FlashScaleApplication.java
@@ -0,0 +1,13 @@
+package sumit.flashscale.flash_scale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashScaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashScaleApplication.class, args);
+	}
+
+}
---
+++ flash_scale/src/test/java/sumit/flashscale/flash_scale/FlashScaleApplicationTests.java
@@ -0,0 +1,13 @@
+package sumit.flashscale.flash_scale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashScaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
